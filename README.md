30581dbb3eeeb78885125a4a4b58f904cb0970e9
Shepherd Nkomo <shepxp@gmail.com>
2012-11-24 10:19:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My first sample application: sample application
+
+This is the application for 
+[*The Stinkyinky Republic*] (http://www.shepherdnkomo.co.za)
+by [Shepherd Nkomo] (shepxp@gmail.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '47ac52cbd4f6811fbfe19e03a82d818c6c28deb319280e9f8121ceb9ae79d09d93d9722baca4c7cbee8e4886230dd862e73612595a5b399518e6841daa3ba885'
