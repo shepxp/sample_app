39e34a73e26c729ddeb4bf0a11691c0d328a31d6
Shepherd Nkomo <shepxp@gmail.com>
2012-12-29 18:25:15 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory   :user do
+    name    "Shepherd Nkomo"
+    email   "shepxp@gmail.com"
+    password "stinkyinky"
+    password_confirmation "stinkyinky"
+   end
+ end
+    
