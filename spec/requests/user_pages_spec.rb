4b959dd4d5a2e4bf527a05f5bedb22dea5d0d350
Shepherd Nkomo <shepxp@gmail.com>
2012-12-30 12:38:41 +0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,11 +27,15 @@ describe "User pages" do
         fill_in "Name",       with: "Example User"
         fill_in "Email",      with: "user@example.com"
         fill_in "Password",   with: "foobar"
-        fill_in "Confirmaation", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
       
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        it { should have_link('Sign out')}
       end
     end
     
